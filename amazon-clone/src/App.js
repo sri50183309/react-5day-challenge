863aa0a9335b2c9c8981a8c61776bad3d258b922
Sriram <sriram.mahalingam@gmail.com>
2020-09-08 19:15:03 -0400

    added basic home in to App js

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from './Header'
+import Home from './Home';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       {/*Header*/}
       <Header />
       {/*Home*/}
+      <Home/>
     </div>
   );
 }
