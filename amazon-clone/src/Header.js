a9a5b513b4de36a740a3fe442ae23395590116d9
Sriram <sriram.mahalingam@gmail.com>
2020-09-08 19:10:23 -0400

    added material ui icons

--- amazon-clone/src/Header.js
+++ amazon-clone/src/Header.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import './Header.css'
+import SearchIcon from '@material-ui/icons/Search'
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket'
 function Header() {
     return (
         <div className="header">
             <img alt="Amazon Logo White" src="https://www.nicepng.com/png/full/228-2281836_vault-logo-available-amazon-app-store.png" className="header_logo" />
             <div className="header_search">
                 <input type="search" className="header_searchInput" />
+                <SearchIcon className="header_searchIcon"/>
             </div>
             <div className="header_nav">
                 <div className="header_option">
@@ -33,6 +36,7 @@ function Header() {
                     </span>
                 </div>
                 <div className="header_optionBasket">
+                    <ShoppingBasketIcon/>
                     <span className="header_optionLineTwo header_basketCount">0</span>
                 </div>
             </div>
