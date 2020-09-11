bea54ae756841d64a11f10b8b0f0138a3e4f1735
Sriram <sriram.mahalingam@gmail.com>
2020-09-11 06:18:37 -0400

    fixed UI Alignment on checkout and header page

--- amazon-clone/src/CheckoutProduct.js
+++ amazon-clone/src/CheckoutProduct.js
@@ -1,37 +1,39 @@
-import React from "react"
-import "./Checkout.css"
-import  Subtotal from  "./Subtotal"
-import {useStateValue } from './StateProvider'
-function CheckoutProduct({id, image, title, price, rating}){
-    const [{basket}, dispatch] = useStateValue()
+import React from 'react';
+import './CheckoutProduct.css'
+import { useStateValue } from "./StateProvider";
+
+function CheckoutProduct({ id, image, title, price, rating }) {
+    const [{ basket }, dispatch] = useStateValue();
 
     const removeFromBasket = () => {
-        // remove the item from basket
+        // remove the item from the basket
         dispatch({
-                type: 'REMOVE_FROM_BASKET',
-                id: id
-            }
-        )
+            type: 'REMOVE_FROM_BASKET',
+            id: id,
+        })
     }
 
     return (
-        <div className="checkoutProduct">
-            <img className="checkoutProduct_image" src={image} ></img>
-            <div className="checkoutProduct_info">
-                <p className="checkoutProduct_title">{title}</p>
-                <p className="checkoutProduct_price">
+        <div className='checkoutProduct'>
+            <img className='checkoutProduct__image' src={image} />
+
+            <div className='checkoutProduct__info'>
+                <p className='checkoutProduct__title'>{title}</p>
+                <p className="checkoutProduct__price">
                     <small>$</small>
                     <strong>{price}</strong>
                 </p>
-           
-                <div className="checkoutProduct_rating">
-                        {Array(rating).fill().map((_, i) => (
-                            <p>🌟</p>
-                        ))}
+                <div className="checkoutProduct__rating">
+                    {Array(rating)
+                    .fill()
+                    .map((_, i) => (
+                        <p>🌟</p>
+                    ))}
                 </div>
                 <button onClick={removeFromBasket}>Remove from Basket</button>
             </div>
         </div>
     )
 }
+
 export default CheckoutProduct
--- amazon-clone/src/Header.js
+++ amazon-clone/src/Header.js
@@ -1,63 +1,63 @@
-import React from 'react'
-import './Header.css'
-import SearchIcon from '@material-ui/icons/Search'
-import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket'
-import { Link } from 'react-router-dom'
-import { useStateValue } from './StateProvider'
-import { auth } from './firebase';
-function Header() {
-    const [{basket,user}, dispatch] = useStateValue()
+import React from "react";
+import "./Header.css";
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
+import { Link } from "react-router-dom";
+import { useStateValue } from "./StateProvider";
+import { auth } from "./firebase";
 
-    const handleAuthenticaton = () => {
-        if (user) {
-          auth.signOut();
-        }
-      }
-    return (
-        <div className="header">
-           <Link to="/">
-                <img alt="Amazon Logo White" src="https://www.nicepng.com/png/full/228-2281836_vault-logo-available-amazon-app-store.png" className="header_logo" />
-            </Link>
-            <div className="header_search">
-                <input type="search" className="header_searchInput" />
-                <SearchIcon className="header_searchIcon"/>
-            </div>
-            <div className="header_nav">
-                <Link to={!user && '/login'}>
-                <div onClick={handleAuthenticaton} className="header_option">
-                    <span className="header_optionLineOne">
-                        Hello Guest
-                    </span>
-                    <span className="header_optionLineTwo">
-                    {user ? 'Sign Out' : 'Sign In'}
-                    </span>
-                </div>
-                </Link>
-                <div className="header_option">
-                <span className="header_optionLineOne">
-                        Returns
-                    </span>
-                    <span className="header_optionLineTwo">
-                        &amp; Orders
-                    </span>
-                </div>
-                <div className="header_option">
-                <span className="header_optionLineOne">
-                       Your 
-                    </span>
-                    <span className="header_optionLineTwo">
-                        prime
-                    </span>
-                </div>
-                <Link to="/checkout">
-                    <div className="header__optionBasket">
-                        <ShoppingBasketIcon />
-                        <span className="header__optionLineTwo header__basketCount">{basket?.length}</span>
-                    </div>
-                </Link>
-            </div>
+function Header() {
+  const [{ basket, user }, dispatch] = useStateValue();
+
+  const handleAuthenticaton = () => {
+    if (user) {
+      auth.signOut();
+    }
+  }
+
+  return (
+    <div className="header">
+      <Link to="/">
+        <img
+          className="header__logo"
+          src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+        />
+      </Link>
+
+      <div className="header__search">
+        <input className="header__searchInput" type="text" />
+        <SearchIcon className="header__searchIcon" />
+      </div>
+
+      <div className="header__nav">
+        <Link to={!user && '/login'}>
+          <div onClick={handleAuthenticaton} className="header__option">
+            <span className="header__optionLineOne">Hello Guest</span>
+            <span className="header__optionLineTwo">{user ? 'Sign Out' : 'Sign In'}</span>
+          </div>
+        </Link>
+
+        <div className="header__option">
+          <span className="header__optionLineOne">Returns</span>
+          <span className="header__optionLineTwo">& Orders</span>
         </div>
-    )
+
+        <div className="header__option">
+          <span className="header__optionLineOne">Your</span>
+          <span className="header__optionLineTwo">Prime</span>
+        </div>
+
+        <Link to="/checkout">
+          <div className="header__optionBasket">
+            <ShoppingBasketIcon />
+            <span className="header__optionLineTwo header__basketCount">
+              {basket?.length}
+            </span>
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
