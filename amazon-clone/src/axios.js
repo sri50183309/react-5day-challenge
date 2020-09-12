f911b82f2d2dd3ddfa702cbe0921b5cd8b4abc63
Sriram <sriram.mahalingam@gmail.com>
2020-09-12 08:49:12 -0400

    updated backend url with cloudfunction in firebase

--- amazon-clone/src/axios.js
+++ amazon-clone/src/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios"
 
 const instance = axios.create({
-    baseURL: "http://localhost:5001/challenge-3fe5b/us-central1/api" //baseurl is advantage of axios API URL (cloud function url)
+    //baseURL: "http://localhost:5001/challenge-3fe5b/us-central1/api" //baseurl is advantage of axios API URL (cloud function url)
+    baseURL: "https://us-central1-challenge-3fe5b.cloudfunctions.net/api"
+
     //create back end to support 
 });
 
